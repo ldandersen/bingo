3fce694c1ecb53722808897b84902c15758f7a54
Buzz Andersen <buzz@andersen.buzz>
2018-09-13 18:28:36 -0400

    Start working on grid css.

--- Sources/App/Controllers/BingoCardController.swift
+++ Sources/App/Controllers/BingoCardController.swift
@@ -7,8 +7,33 @@
 
 import Vapor
 
+struct Tile: Encodable {
+    let title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
+
 class BingoCardController {
     func generateCard(_ req: Request) throws -> Future<View> {
-        return try req.view().render("hello", ["name": "Leaf"])
+        /*let jsonFileURL = Bundle.main.url(forResource: "tiles", withExtension: "json", subdirectory: "Data")
+        let jsonData = try Data(contentsOf: jsonFileURL!)
+        
+        let tilesJSON = try JSONSerialization.jsonObject(with: jsonData, options: [])*/
+        
+        let tiles = [Tile(title: "Tile 1"),
+                     Tile(title: "Tile 2"),
+                     Tile(title: "Tile 3"),
+                     Tile(title: "Tile 4"),
+                     Tile(title: "Tile 5"),
+                     Tile(title: "Tile 6"),
+                     Tile(title: "Tile 7"),
+                     Tile(title: "Tile 8"),
+                     Tile(title: "Tile 9"),
+                     Tile(title: "Tile 10"),
+                     Tile(title: "Tile 11")]
+        
+        return try req.view().render("card", ["tiles" : tiles])
     }
 }
